15217eaf2c270e00b2485197be82bc6f3566c95c
Simon van den Berg <simonschrotenberg@gmail.com>
2023-04-19 16:01:54 +0200

    adde y axis

--- motorTest/motorTest.ino
+++ motorTest/motorTest.ino
@@ -1,7 +1,14 @@
 #define X_MOTOR_DIRECTION 12
 #define X_MOTOR_SPEED 3
+#define Y_MOTOR_DIRECTION 13
+#define Y_MOTOR_SPEED 11
 
-
+enum axis
+{
+    X,
+    Y,
+    Z
+};
 
 void setup()
 {
@@ -12,33 +19,72 @@ void setup()
 
 void loop()
 {
-    setXMotorDirection(true);
-    setXMotorSpeed(255);
+    setMotorDirection(X, true);
+    setMotorSpeed(X, 255);
     delay(1000);
-    setXMotorDirection(false);
-    setXMotorSpeed(255);
+    setMotorDirection(X, false);
+    setMotorSpeed(X, 255);
     delay(1000);
+    setMotorSpeed(X, 0);
+
+    setMotorDirection(Y, true);
+    setMotorSpeed(Y, 255);
+    delay(1000);
+    setMotorDirection(Y, false);
+    setMotorSpeed(Y, 255);
+    delay(1000);
+    setMotorSpeed(Y, 0);
 }
 
 /**
- *  set the speed of the right motor
+ *  set the speed of the motor
+ * @param axis X, Y or Z
  * @param speed 0 - 255
  */
-void setXMotorSpeed(int speed)
+void setMotorSpeed(axis axis, int speed)
 {
-    analogWrite(X_MOTOR_SPEED, speed);
+    switch (axis)
+    {
+    case X:
+        analogWrite(X_MOTOR_SPEED, speed);
+        break;
+    case Y:
+        analogWrite(Y_MOTOR_SPEED, speed);
+        break;
+    case Z:
+        break;
+    default:
+        break;
+    }
 }
 
 /**
  *   set the direction of the right motor
+ *   @param axis X, Y or Z
  *   @param direction true = forward, false = backward
  */
-void setXMotorDirection(bool direction)
+void setMotorDirection(axis axis, bool direction)
 {
+    int target = 0;
+    switch (axis)
+    {
+    case X:
+        target = X_MOTOR_DIRECTION;
+        break;
+    case Y:
+        target = Y_MOTOR_DIRECTION;
+        break;
+    case Z:
+
+        break;
+    default:
+        break;
+    }
+
     if (direction)
     {
-        digitalWrite(X_MOTOR_DIRECTION, HIGH);
+        digitalWrite(target, HIGH);
         return;
     }
-    digitalWrite(X_MOTOR_DIRECTION, LOW);
+    digitalWrite(target, LOW);
 }
