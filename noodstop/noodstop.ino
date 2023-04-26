ea58e6f2a16a68d7f468b3036f2365e7f41a78ac
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2023-04-26 11:10:01 +0200

    noodstop gemaakt en reset via serial

--- noodstop/noodstop.ino
+++ noodstop/noodstop.ino
@@ -1,68 +1,42 @@
 const int interruptPin = 2;
-int resetNoodStop = 0;
 bool noodstopstate = false;
 
-
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), noodstop, FALLING);
 
   Serial.begin(9600);
-
 }
 
-void loop()
-{
-
-  // put your main code here, to run repeatedly:
-  noodstoploop(); 
-  delay(100);
-  if(noodstopstate){
-    Serial.println("noodstop aan");
+void loop() {
+  if (noodstopstate) {
+    noodstoploop();
+    return;
   }
-  else{
-    Serial.println("noodstop uit");
-  }
+  Serial.println("noodstop niet actief");
 }
-
-
-
-void noodstop()
-{
+/**
+ * noodstop interupt function
+*/
+void noodstop() {
   noodstopstate = true;
   Serial.println("noodstop knop ingedrukt");
-detachInterrupt(digitalPinToInterrupt(interruptPin));
-
-
-  
 }
-void noodstoploop()
-{
+/**
+ *noodstop reset function
+  */ 
+void noodstoploop() {
 
 
-  if (noodstopstate)
-  {
-    while (((resetNoodStop + 5000) > millis()) || resetNoodStop == 0)
-    {
-     Serial.println("noodstop aan");
-      bool noodstopknop = !digitalRead(interruptPin);
-      if (noodstopknop && resetNoodStop == 0)
-      {
-        resetNoodStop = millis();
-      }
-      else if (noodstopknop == false)
-      {
-        resetNoodStop = 0;
-      }
-    }
-    noodstopstate = false;
-    resetNoodStop = 0;
-    while(!digitalRead(interruptPin)){}
-
-    attachInterrupt(digitalPinToInterrupt(interruptPin), noodstop, FALLING);
-
+  String serialInput;
+  if (Serial.available() > 0) {
+    serialInput = Serial.readString();
   }
-  
+  if (serialInput == "reset") {
+    Serial.println("noodstop gereset");
+    noodstopstate = false;
+  }
+
+  Serial.println("noodstop actief");
 }
